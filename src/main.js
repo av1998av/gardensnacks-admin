b3c931c8b9f754393ef8fae2bca5c43962db5eea
av1998av <av1998av@gmail.com>
2023-12-12 22:29:14 +0530

    adding bootstrap and toast library

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
 import App from './App.vue'
 import router from './router'
+
+const options = {
+	
+};
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Toast, options);
 
 app.mount('#app')
