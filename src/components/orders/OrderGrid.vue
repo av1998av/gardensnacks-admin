abd16ee77b6e6adaab248f21e66b5abd1496ef8e
av1998av <av1998av@gmail.com>
2023-12-19 22:49:55 +0530

    adding modals and other info to Orders page

--- src/components/orders/OrderGrid.vue
+++ src/components/orders/OrderGrid.vue
@@ -2,29 +2,60 @@
   <table class="table mt-5">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">OrderId</th>
+      <th scope="col">Profile</th>
+      <th scope="col">Address</th>
+      <th scope="col">Bill</th>
+      <th scope="col">Status</th>
+      <th scope="col">Shipping Id</th>
+      <th scope="col">Shiping info</th>
     </tr>
   </thead>
   <tbody>
     <tr v-for="item in summary" :key="item.id">
-      <th scope="row">1</th>
       <td>{{item.orderId}}</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <td><button class="btn btn-primary btn-sm" v-on:click="loadUserProfile(item.User)">Profile</button></td>
+      <td><button class="btn btn-success btn-sm" v-on:click="loadUserAddress(item.Address)">Address</button></td>
+      <td><button class="btn btn-danger btn-sm">Bill</button></td>
+      <td>{{item.status}}</td>
+      <td>GPM00123000123</td>
+      <td><button class="btn btn-warning btn-sm">Add Shipping info</button></td>
     </tr>    
   </tbody>
 </table>
-<user-profile></user-profile>
+<w-overlay v-model="showUserOverlay">
+	<user-profile :user="currUser"></user-profile>
+</w-overlay>
+
+<w-overlay v-model="showAddressOverlay">
+	<user-address :address="currAddress"></user-address>
+</w-overlay>
 </template>
 
 <script>
 import UserProfile from './UserProfile.vue';
+import UserAddress from './UserAddress.vue';
 export default {
+  data(){
+    return {
+      showUserOverlay: false,
+      showAddressOverlay: false,
+      currUser: null,
+      currAddress : null,
+    }
+  },
 	props: ['summary'],
-  components : {UserProfile}
+  components : {UserProfile, UserAddress},
+  methods: {
+    loadUserProfile(user){
+      this.currUser = user;
+      this.showUserOverlay = true;
+    },
+    loadUserAddress(address){
+      this.currAddress = address;
+      this.showAddressOverlay = true;
+    }
+  }
 }
 </script>
 
