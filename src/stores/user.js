ccf4818057c8304d557bcdb76fdeedb4b6a2a5f1
av1998av <av1998av@gmail.com>
2024-09-28 01:07:52 +0530

    add server side pagination for users table

--- src/stores/user.js
+++ src/stores/user.js
@@ -9,6 +9,8 @@ const toast = useToast()
 export const useUserStore = defineStore("user",{
     state: () => ({
 		token: null,
+		users: [],
+		total: 0
 	}),
     getters: {
 		getToken(state){
@@ -16,6 +18,12 @@ export const useUserStore = defineStore("user",{
 		},
 		isLoggedIn(state){
 			return state.token != null;
+		},
+		getUsers(state){
+			return state.users
+		},
+		getTotal(state){
+			return state.total
 		}
 	},
     actions: {
@@ -41,7 +49,41 @@ export const useUserStore = defineStore("user",{
 				toast.error(error.response.data.message);
                 return false;
 			}
-		}
+		},
+		async fetchUsers(token, page, size){
+			try {
+				let config = {
+					headers: {
+						'Authorization' : token
+					}
+				}
+				var request_url = api_url + '/users?';
+				if(page){
+					request_url += 'page='+page+"&"
+				}
+				if(size){
+					request_url += 'size='+size+'&'
+				}
+				request_url = request_url.slice(-1) == '&' ? request_url.slice(0,-1) : request_url; 
+				var response = await axios.get(request_url, config);
+                if (response.status == '200'){
+					this.users = response.data['users']['rows'];
+					this.total = response.data['users']['count'];
+					console.log(this.users);
+                    toast.info('Users fetched');
+                    return true;   
+                }
+                else{
+                    toast.error(response.data['message']);                    
+                    return false;
+                }
+			}
+			catch(error){
+				console.log(error);
+				toast.error(error.response.data.message);
+                return false;
+			}
+		},
 	},
 	persist: true,
 })
--- src/views/UsersPage.vue
+++ src/views/UsersPage.vue
@@ -1,15 +1,73 @@
 <template>
 <div class="mx-auto" style="width:95%">
   <div class="mt-4"><p class="h3">Users</p></div>
-  <users-grid class="mt-3"></users-grid>
+  <!-- <users-grid class="mt-3"></users-grid> -->
+   <w-table
+    :headers="table.headers"
+    :items="table.users"
+    fixed-headers
+    fixed-footer
+    :fetch="fetch"
+    :pagination="table.pagination"
+    :loading="table.loading"
+    style="max-height: 1000px"
+  >
+  </w-table>
 </div>
   <!-- <users-grid></users-grid> -->
 </template>
 
 <script>
 import UsersGrid from '../components/users/UsersGrid.vue';
+import { mapActions, mapState } from 'pinia'
+import { useUserStore } from '../stores/user';
 export default {
   components : { UsersGrid },
+  data(){
+    return {
+      table: {
+        headers: [
+          { label: 'ID', key: 'id' },
+          { label: 'Email Or Phone', key: 'emailOrPhone' },
+          { label: 'Orders', key: '' },
+          { label: 'Last Order', key: '' },
+          { label: 'Addresses', key: '' },
+        ],
+        users: [],
+        loading: false,
+        pagination: {
+          page: 1,
+          itemsPerPage: 20,
+          total: this.getTotal()
+        }
+      }
+    }
+  },
+  methods: {
+    ...mapActions(useUserStore, ['fetchUsers']),
+    ...mapState(useUserStore, {
+      getToken: 'getToken'
+    }),
+    ...mapState(useUserStore, {
+      getUsers: 'getUsers',
+      getTotal: 'getTotal'
+    }),
+    async fetch ({ page, start, end, total, itemsPerPage, sorting }) {           
+      try{
+				this.table.loading = 'header'
+        var token = this.getToken();
+				var success = await this.fetchUsers(token,page,itemsPerPage);
+        if(success){
+            this.table.pagination.total = this.getTotal();
+            this.table.loading = false;
+            this.table.users = this.getUsers();
+        }
+      }
+      catch(error){
+          console.log(error);
+      }
+    },
+  }
 }
 </script>
 
